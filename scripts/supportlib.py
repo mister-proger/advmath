53b83f139acd2f7891c702680630d05cc22e4877
mister-proger <84221561+mister-proger@users.noreply.github.com>
2023-02-14 22:02:08 +0300

    Delete supportlib.py

--- scripts/supportlib.py
+++
@@ -1,5 +0,0 @@
-def mul(list_of_numbers):
-    return_number = 1
-    for n in range(len(list_of_numbers)):
-        return_number = return_number * int(list_of_numbers[n])
-    return return_number
